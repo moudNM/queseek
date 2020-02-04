bb6cbe4e7af6c4fafa36e6248b3a007d280f5ce5
Nur Muhammad <moud@live.com.sg>
2020-02-04 20:30:17 +0800

    new update

--- main.py
+++ main.py
@@ -2,6 +2,3 @@ from app import app
 
 if __name__ == "__main__":
 	app.run(host='127.0.0.1', port=8080, debug =True, use_reloader=True)
-
-# socketio.run(app, host='0.0.0.0', port=5000)
-# https://www.youtube.com/watch?v=eayijy9f240
